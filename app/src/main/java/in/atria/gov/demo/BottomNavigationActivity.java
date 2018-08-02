447f6017db6db0c8a6396ad3367a87706d81705f
madeinatria <whoknows_0>
2018-08-02 12:53:24 +0530

    Basic setup done !!

--- app/src/main/java/in/atria/gov/demo/BottomNavigationActivity.java
+++ app/src/main/java/in/atria/gov/demo/BottomNavigationActivity.java
@@ -1,15 +1,19 @@
 package in.atria.gov.demo;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import in.atria.gov.demo.Fragments.HomeFragment;
+import in.atria.gov.demo.Fragments.ProfileFragment;
 
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -18,29 +22,16 @@ public class BottomNavigationActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        Fragment selectedFragment = null;
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
+
+
+        ActionBar actionBar;
+        actionBar= getSupportActionBar();
+        actionBar.hide();
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
@@ -52,4 +43,51 @@ public class BottomNavigationActivity extends AppCompatActivity {
 
     }
 
+    private void openFragment(final Fragment fragment)   {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frame_layout, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            HomeFragment frg1 = new HomeFragment ();
+
+            switch (item.getItemId()) {
+
+                case R.id.navigation_dashboard:
+                    openFragment(frg1);
+                    break;
+
+                case R.id.navigation_contact:
+                    startActivity(new Intent(getApplicationContext(),ContactActivity.class));
+                    break;
+
+                case R.id.navigation_report:
+                    startActivity(new Intent(getApplicationContext(),ReportActivity.class));
+                    break;
+
+                case R.id.navigation_profile:
+                    ProfileFragment frg12 = new ProfileFragment ();
+                    openFragment(frg12);
+                    break;
+
+
+                default:
+                    openFragment(frg1);
+                    break;
+
+
+
+            }
+
+            return true;
+        }
+    };
 }
---
+++ app/src/main/java/in/atria/gov/demo/Fragments/ProfileFragment.java
@@ -0,0 +1,33 @@
+package in.atria.gov.demo.Fragments;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import in.atria.gov.demo.R;
+
+/**
+ * Created by suryamurugan on 4/4/18.
+ */
+
+public class ProfileFragment extends Fragment {
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.profile_fragment,null);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+
+}
