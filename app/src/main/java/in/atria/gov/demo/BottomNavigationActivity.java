fb89f190f8d5510efecded9111e154f7237410d0
madeinatria <whoknows_0>
2018-08-02 11:43:06 +0530

    YAY :)

---
+++ app/src/main/java/in/atria/gov/demo/BottomNavigationActivity.java
@@ -0,0 +1,55 @@
+package in.atria.gov.demo;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import in.atria.gov.demo.Fragments.HomeFragment;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class BottomNavigationActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        Fragment selectedFragment = null;
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new HomeFragment()).commit();
+        navigation.getMenu().getItem(0).setChecked(true);
+
+
+    }
+
+}
